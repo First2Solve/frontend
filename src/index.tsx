24482b81484cf8ad57f5db54d720194f90e04189
Aqib Shah <ashah023@fiu.edu>
2019-02-09 00:41:17 -0500

    Add routing support

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Hello } from './components/Hello';
+// import { Hello } from './components/Hello';
+import Router from './routes';
 
 ReactDOM.render(
-  <Hello compiler="Typescript" framework="React" />,
+  <Router />,
   document.querySelector('#app')
 )
